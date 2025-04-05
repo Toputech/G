ebbd5606cabd5eb55e127c63c073d1b5188380f9
Toputech <168991681+Toputech@users.noreply.github.com>
2025-04-05 06:31:44 +0300

    Update yts.js

--- commandes/yts.js
+++ commandes/yts.js
@@ -3,7 +3,7 @@ const { zokou } = require(__dirname + "/../framework/zokou");
 const { format } = require(__dirname + "/../framework/mesfonctions");
 const os = require('os');
 const moment = require("moment-timezone");
-const settings = require(__dirname + "/../set");
+const settings = require(__dirname + "/../config");
 
 
 zokou({nomCom:"restart",categorie:"Mods",reaction:"📴"},async(dest,z,com)=>{
